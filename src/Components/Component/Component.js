512d06dd178f63a355a66f5d1b57a8c737d7a94b
ali.daeiلهف <ali.daei.dbm@gmail.com>
2022-06-06 00:12:46 +0430

    fix intial data amounts

--- src/Components/Component/Component.js
+++ src/Components/Component/Component.js
@@ -32,9 +32,9 @@ const Component = (props) => {
             id: uuid(),
             labels: ['index1', 'index2', 'index3', 'index4', 'index5', 'index6'],
             label: "Its Sample",
-            data: [65, 59, 80, 81, 56, 55, 40],
-            backgroundColor: ["#ff6384", "#ff9f40", "#ffcd56", "#4bc0c0", "#36a2eb", "#9966ff", "#c9cbcf"],
-            borderColor: ["#ff6384", "#ff9f40", "#ffcd56", "#4bc0c0", "#36a2eb", "#9966ff", "#c9cbcf"]
+            data: [65, 59, 80, 81, 56, 55],
+            backgroundColor: ["#ff6384", "#ff9f40", "#ffcd56", "#4bc0c0", "#36a2eb", "#9966ff"],
+            borderColor: ["#ff6384", "#ff9f40", "#ffcd56", "#4bc0c0", "#36a2eb", "#9966ff"]
         }
         setBartChartObj(() => newChart)
         const updatedList = [...chartList, barChartObj]
